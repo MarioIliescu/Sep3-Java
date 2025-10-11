26001a525f627e1c6a7a2f9b9c86e8596e695253
Mario Iliescu <355142@via.dk>
2025-10-11 18:55:07 +0200

    first commit

---
+++ src/main/java/dk/via/Sep3JavaApplication.java
@@ -0,0 +1,13 @@
+package dk.via;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep3JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sep3JavaApplication.class, args);
+    }
+
+}
